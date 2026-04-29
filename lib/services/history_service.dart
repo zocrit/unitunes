8b921b15164203c1c4c7eb468481574d656030c3
Cristian Zoz <cristian@zoz.dev>
2026-04-29 18:34:18 +0200

    fix: log history entry parse failures

--- lib/services/history_service.dart
+++ lib/services/history_service.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:flutter/foundation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../models/history_entry.dart';
 
@@ -23,7 +24,8 @@ class HistoryService {
   static HistoryEntry? _tryParse(Map<String, dynamic> json) {
     try {
       return HistoryEntry.fromJson(json);
-    } catch (_) {
+    } catch (e) {
+      debugPrint('history entry parse failed: $e');
       return null;
     }
   }
